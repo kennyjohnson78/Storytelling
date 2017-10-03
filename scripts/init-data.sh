2f12b99f9e5827a74bcc861dcf2356ea87fc8d0f
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-10-03 12:16:43 +0200

    fix

--- scripts/init-data.sh
+++ scripts/init-data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 mongorestore  --db storytelling-dev --collection slides ../dump/storytelling-dev/slides.bson
+mongorestore  --db storytelling-dev --collection images ../dump/storytelling-dev/images.bson
 
 mongorestore  --db storytelling --collection slides ../dump/storytelling-dev/slides.bson
+mongorestore  --db storytelling --collection images ../dump/storytelling-dev/images.bson
