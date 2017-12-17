fa9630094bad8ebed6204d53bcf0e53276aa90dc
Foad Ardalan <foad.ardalan@outlook.fr>
2017-12-17 16:52:00 +0100

    nx intergation

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,12 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
-// Karma configuration
-module.exports = function (karmaConfig) {
-  karmaConfig.set({
+const { makeSureNoAppIsSelected } = require('@nrwl/schematics/src/utils/cli-config-utils');
+// Nx only supports running unit tests for all apps and libs.
+makeSureNoAppIsSelected();
+
+module.exports = function (config) {
+  config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
@@ -16,16 +19,6 @@ module.exports = function (karmaConfig) {
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    // List of files / patterns to load in the browser
-    files: [
-      { pattern: './client/test.ts', watched: false }
-    ],
-    preprocessors: {
-      './client/test.ts': ['@angular/cli']
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
-    },
     coverageIstanbulReporter: {
       reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
@@ -33,33 +26,12 @@ module.exports = function (karmaConfig) {
     angularCli: {
       environment: 'dev'
     },
-    // Test results reporter to use
-    // Possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: karmaConfig.angularCli && karmaConfig.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
-              : ['progress', 'kjhtml'],
-    // Web server port
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
-    // Enable / disable colors in the output (reporters and logs)
     colors: true,
-    // Level of logging
-    // Possible values: karmaConfig.LOG_DISABLE || karmaConfig.LOG_ERROR || karmaConfig.LOG_WARN || karmaConfig.LOG_INFO || karmaConfig.LOG_DEBUG
-    logLevel: karmaConfig.LOG_INFO,
-    // Enable / disable watching file and executing tests whenever any file changes
+    logLevel: config.LOG_INFO,
     autoWatch: true,
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
     browsers: ['Chrome'],
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
-    // Continuous Integration mode
-    // If true, it capture browsers, run tests and exit
-    singleRun: true
+    singleRun: false
   });
 };
